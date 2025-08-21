1b4d9592a1a8e9397cd4e746fb658d4072e91b40
Dmytro_Gunko <dimagunko99@gmail.com>
2025-08-21 13:27:18 +0200

    added metadata, generateMetadata, font

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,23 @@
+import { Metadata } from 'next';
 import css from './NotFound.module.css';
+
+export const metadata: Metadata = {
+  title: 'Page Not Found',
+  description: ' The page you are looking for does not exist.',
+  openGraph: {
+    title: '404 - Page Not Found',
+    description: 'This page could not be found',
+    url: 'https://07-routing-nextjs-sage.vercel.app/404',
+    images: [
+      {
+        url: 'https://07-routing-nextjs-sage.vercel.app/404',
+        width: 1200,
+        height: 630,
+        alt: '404 Page not found',
+      },
+    ],
+  },
+};
 
 const NotFound = () => {
   return (
